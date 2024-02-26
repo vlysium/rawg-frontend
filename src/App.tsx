7cc53e943bb75973c9dfe01e87f50b1f26344ab1
Victor Ly <ly.victor2000@gmail.com>
2024-02-26 11:35:56 +0100

    implement platform selector

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,28 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/usePlatforms";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const handleGenreSelect = (genre: Genre) => {
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+
+  const handleSelectGenre = (genre: Genre) => {
     if (selectedGenre?.id === genre.id) {
       setSelectedGenre(null);
       return;
     }
 
     setSelectedGenre(genre);
+  }
+
+  const handleSelectPlatform = (platform: Platform) => {
+    if (selectedPlatform?.id === platform.id) {
+      setSelectedPlatform(null);
+      return;
+    }
+
+    setSelectedPlatform(platform);
   }
 
   return (
@@ -34,12 +46,21 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem gridArea={"aside"} pt={4}>
-          <GenreList onSelectedGenre={handleGenreSelect} selectedGenre={selectedGenre} />
+          <GenreList
+            onSelectedGenre={handleSelectGenre}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Show>
       <GridItem gridArea={"main"} p={4}>
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector
+          onSelectedPlatform={handleSelectPlatform}
+          selectedPlatform={selectedPlatform}
+        />
+        <GameGrid
+          selectedGenre={selectedGenre}
+          selectedPlatform={selectedPlatform}
+        />
       </GridItem>
     </Grid >
   );
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,8 +1,13 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
-import usePlatforms from "../hooks/usePlatforms";
+import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectedPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
   const { data: platforms, error } = usePlatforms();
 
   if (error) return null;
@@ -10,11 +15,14 @@ const PlatformSelector = () => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platform
+        {selectedPlatform ? selectedPlatform.name : "Platform"}
       </MenuButton>
       <MenuList>
         {platforms.map((platform) => (
-          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+          <MenuItem
+            key={platform.id}
+            onClick={() => onSelectedPlatform(platform)}
+          >{platform.name}</MenuItem>
         ))}
       </MenuList>
     </Menu>
